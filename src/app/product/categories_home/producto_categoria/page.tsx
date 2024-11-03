8e1175c8ee607157b9bef34b039d476c2f5fe29c
CamiloCortes1208 <juanc.cortesd@uqvirtual.edu.co>
2024-11-03 15:40:57 -0500

    Creación y modificación de las páginas de categoría

---
+++ src/app/product/categories_home/producto_categoria/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import ProductoCategoria from '@/components/tarjetas/producto-categoria';
+import productos from '@/data/products';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+export default function Categories_home() {
+
+    productos
+
+    return (
+        <div className="container text-center">
+            <p className="fs-1 fw-bold">Productos de la categoría: Organizar</p>
+            <div className="row row-cols-4 row-cols-md-4 g-4">
+                {productos.map((producto) => (
+                    <div className="col" key={producto.id}>
+                        <ProductoCategoria imagen={producto.imagen} nombre={producto.nombre} precio={producto.precio}/>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
