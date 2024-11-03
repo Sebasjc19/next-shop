271539e3042767224e6ab491040f72d1a25e30bd
CamiloCortes1208 <juanc.cortesd@uqvirtual.edu.co>
2024-11-03 12:33:38 -0500

    Se agregan el componente fila-producto.tsx

--- src/components/fila-producto.tsx
+++ src/components/fila-producto.tsx
@@ -3,19 +3,19 @@
 import Link from "next/link"
 
 interface ProductoPerfilProps {
-    id: string;
+    id: number;
     nombre: string;
-    precio: string;
-    stock: number;
+    precio: number;
+    cantidad: number;
 }
 
-export default function Fila_producto({id, nombre, precio, stock}: ProductoPerfilProps) {
+export default function Fila_producto({id, nombre, precio, cantidad}: ProductoPerfilProps) {
     return (
         <tr>
             <th scope="row">{id}</th>
             <td>{nombre}</td>
             <td>{precio}</td>
-            <td>{stock}</td>
+            <td>{cantidad}</td>
             <td>
                 <Link href="/product/product_information" passHref><button type="button" className="btn btn-info me-2">Ver Detalles</button></Link>
                 <Link href="/product/update_product" passHref><button type="button" className="btn btn-primary me-2">Actualizar</button></Link>
