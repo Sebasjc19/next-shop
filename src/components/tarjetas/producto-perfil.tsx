9889879c22e0f71bf19c5bcf608519ae47cc5b1a
CamiloCortes1208 <juanc.cortesd@uqvirtual.edu.co>
2024-11-03 00:59:52 -0500

    Modificación del componente producto-perfil

--- src/components/tarjetas/producto-perfil.tsx
+++ src/components/tarjetas/producto-perfil.tsx
@@ -1,17 +1,24 @@
 "use client"
 
-export default function Producto_perfil() {
+interface ProductoPerfilProps {
+    imagen: string;
+    nombre: string;
+    precio: string;
+    fecha: string;
+}
+
+export default function Producto_perfil({imagen, nombre, precio, fecha }: ProductoPerfilProps) {
     return (
         <div className="card mb-3">
             <div className="row g-0">
                 <div className="col-md-4 d-flex align-items-center">
-                    <img src="https://www.boningtontheatre.co.uk/wp-content/uploads/2024/09/the-wild-robot-668ec3182e643.jpg" className="img-fluid rounded-start" alt="..." />
+                    <img src={imagen} className="img-fluid rounded-start" alt="..." />
                 </div>
-                <div className="col-md-8">
+                <div className="col-md-8 text-start">
                     <div className="card-body">
-                        <h5 className="card-title">Producto 1</h5>
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+                        <h5 className="card-title">{nombre}</h5>
+                        <p className="fs-6">{precio}</p>
+                        <p className="fs-6">{fecha}</p>
                     </div>
                 </div>
             </div>
